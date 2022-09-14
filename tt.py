f631dc0d1a834d68428ec4763c99c4564f3c9dec
zsy0828 <48177103+zsy0828@users.noreply.github.com>
2022-09-14 13:27:01 +0800

    Update tt.py

--- tt.py
+++ tt.py
@@ -2,7 +2,7 @@ import cupy as cp
 import numpy as np
 import torch
 import ThrustRTC as trtc
-
+import CURandRTC as rndrtc
 
 
 class ttt_havetrtc:
@@ -17,4 +17,11 @@ class ttt_havenotrtc:
         self.a = np.array(a)
         self.b = cp.array(a)
         # self.c = trtc.DVCupyVector(self.b)
+        # self.c = torch.tensor(c)
+
+class ttt_haverndrtc:
+    def __init__(self,a,b=None,c=None):
+        self.a = np.array(a)
+        self.b = cp.array(a)
+        self.c = rndrtc.DVRNG()
         # self.c = torch.tensor(c)
